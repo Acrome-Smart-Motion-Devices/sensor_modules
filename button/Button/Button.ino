1b8639164d17050708971cd184a8023654bcd29d
Furkan Kirlangic <furkan.kirlangic@std.yildiz.edu.tr>
2023-05-11 13:18:36 +0300

    Use ATTinyCore as board manager and built-in Wire.h for I2C

--- button/Button/Button.ino
+++ button/Button/Button.ino
@@ -1,27 +1,20 @@
-/* ATtiny Pin 1 =   (RESET) N/U                      
-ATtiny Pin 2    =   ADC3
-ATtiny Pin 3    =   ADC2                          
-ATtiny Pin 4    =   GND
-ATtiny Pin 5    =   I2C SDA on DS1621  & Mosi     
-ATtiny Pin 6    =   Mıso
-ATtiny Pin 7    =   I2C SCK on DS1621  & ADC1       
-ATtiny Pin 8    =   VCC (2.7-5.5V)
- */
-
+#include <Wire.h>
 
 #define I2C_SLAVE_ADDRESS 0x08  //this slave address
-#include <TinyWireS.h>
 #define buttonPin  3
 
 void setup() {
-  TinyWireS.begin(I2C_SLAVE_ADDRESS);
-  TinyWireS.onRequest(requestEvent);
+  Wire.begin(I2C_SLAVE_ADDRESS);
+  Wire.onRequest(requestEvent);
   pinMode(buttonPin, INPUT);
 }
 
 void loop() {
+
 }
 
+
 void requestEvent() {
-  TinyWireS.write(digitalRead(buttonPin)^(0x01));
+  int a = digitalRead(buttonPin)^(0x01);
+  Wire.write(a);
 }
